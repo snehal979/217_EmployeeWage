6cfb608ec3e3455c4fc3f4dda24cd79323291841
snehal979 <113917713+snehal979@users.noreply.github.com>
2022-11-16 20:36:55 +0530

    condition working

---
+++ SwitchCase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class SwitchCase
+    {
+        public static void  FullORpartTimeWithSwitch()
+        {
+            Random random = new Random();
+            int num = random.Next(3);
+
+            string type = "";
+            int PerDailyWage = 20;
+            int DailyHour = 0;
+
+            switch (num)
+            {
+                 case 1:
+                    type ="FullTimeJob";
+                    DailyHour = 8;
+                break;
+
+                case 2:
+                    type ="PartTimeJob";
+                    DailyHour = 4;
+                break;
+
+                default:
+                    type = "Absent";
+                    DailyHour = 0;
+                    break;
+            }
+            int dailyWage = DailyHour * PerDailyWage;
+            Console.WriteLine("employee is {0}  and daily wage is {1}", type, dailyWage);
+        }
+        
+    }
+}
