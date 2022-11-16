4ea78c763f1b0a4624f81a705f473b7982a6bca1
snehal979 <113917713+snehal979@users.noreply.github.com>
2022-11-16 20:34:10 +0530

    MonthlyWage

---
+++ MonthlyWage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class MonthlyWage
+    {
+        public static void MonthlyEmployeeWage()
+        {
+            string type = "";
+            int PerDailyWage = 20;
+            int WorkingDay = 20;
+            int DailyHour = 0;
+            int TotalHrs = 0;
+            for (int i =1; i<=WorkingDay; i++)
+            {
+                Random random = new Random();
+                int num = random.Next(3);
+                switch (num)
+                {
+                    case 1:
+                        type ="FullTimeJob";
+                        DailyHour = 8;
+                        break;
+
+                    case 2:
+                        type ="PartTimeJob";
+                        DailyHour = 4;
+                        break;
+
+                    default:
+
+                        type = "Absent";
+                        DailyHour = 0;
+                        break;
+                }
+                TotalHrs = TotalHrs + DailyHour;
+                Console.WriteLine("employee is {0}  and total hour working day is {1}", type, TotalHrs);
+            }
+            int TotalMonthWage = TotalHrs*PerDailyWage;
+            Console.WriteLine("total month salary {0}" , TotalMonthWage);
+            
+        }
+
+    }
+}
